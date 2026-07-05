482d337e66abf08452666fc521f520c7cdfc838f
AshiiA9 <ashtona.arellano@yahoo.com>
2026-07-04 23:55:09 -0400

    Fix closing div tag in serviceCard HTML

--- ecosystem.js
+++ ecosystem.js
@@ -86,7 +86,7 @@ for (let service of memory.services) {
      "<strong>" + service.customer + "</strong><br>" +
     service.time + "•" + service.date + "<br>" +
     service.serviceType + "•" + service.status + "• $" + service.cost +
-    "</div>"
+    "</div>";
 
 
 
