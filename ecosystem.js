4d4aac7bc6c24c4b073b339e414da048d54cf447
AshiiA9 <ashtona.arellano@yahoo.com>
2026-07-05 12:49:12 -0400

    Add service duration to service card display

--- ecosystem.js
+++ ecosystem.js
@@ -111,7 +111,7 @@ for (let service of memory.services) {
     "<div class='serviceCard'>" +
      "<strong>" + service.customer + "</strong><br>" +
     service.time + " • " + service.date + "<br>" +
-    service.serviceType + " • " + service.status + " • $"+ service.cost +
+    service.serviceType + " • " + service.status + " • $"+ service.cost + " • " + service.duration +
     "</div>";
 }
 serviceList.innerHTML +=
